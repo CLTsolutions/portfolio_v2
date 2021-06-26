fea1453e037110a2b74b6bf6b7ff1e6b997361df
Chelsey Tschida <chelsey.tschida@outlook.com>
2021-06-26 09:14:21 -0400

    fixed link tags in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,22 +7,22 @@ const Navbar = () => {
       <ul className='flex justify-end self-center space-x-5 mr-4'>
         <li>
           {/* home is hero component */}
-          <Link to='/home' /> Home
+          <Link to='/home'>Home</Link>
         </li>
         <li>
-          <Link to='/about' /> About
+          <Link to='/about'>About</Link>
         </li>
         <li>
-          <Link to='/skills' /> Skills
+          <Link to='/skills'>Skills</Link>
         </li>
         <li>
-          <Link to='/resume' /> Resume
+          <Link to='/resume'>Resume</Link>
         </li>
         <li>
-          <Link to='/projects' /> Projects
+          <Link to='/projects'>Projects</Link>
         </li>
         <li>
-          <Link to='/contact' /> Contact
+          <Link to='/contact'>Contact</Link>
         </li>
       </ul>
     </nav>
