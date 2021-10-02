3a01c7d97b152f254f92c99934a2c5fac9c3ba59
Chelsey Tschida <chelsey.tschida@outlook.com>
2021-10-02 12:12:42 -0400

    added mobile dropdown

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,5 @@
 import chelseyPlane from '../assets/ctPlane.JPG'
 import chelseyParis from '../assets/ctParis.JPG'
+import Logo from '../assets/chelseyBrandLogo.png'
 
-export { chelseyParis, chelseyPlane }
+export { chelseyParis, chelseyPlane, Logo }
--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { useLocation } from 'react-router'
 import { NavbarLinks } from '../data'
+import { Logo } from '../../assets/'
+import { AiOutlineBars, AiOutlineClose } from 'react-icons/ai'
 // import '../styles/Navbar.css'
 
 const Navbar = () => {
@@ -11,7 +13,7 @@ const Navbar = () => {
    const navbarMapper = () => {
       return Object.keys(NavbarLinks).map((link, id) => (
          <li
-            className='hover:bg-lavender-dark hover:text-white px-3 py-2 rounded-md uppercase tracking-wider text-gray-700 text-2xl'
+            className='block hover:bg-lavender-dark hover:text-white px-2 py-2 rounded-md uppercase tracking-wider text-gray-700 text-2xl'
             key={id}
          >
             <Link to={NavbarLinks[link]}>{link}</Link>
@@ -19,9 +21,17 @@ const Navbar = () => {
       ))
    }
 
-   const mobile = () => {
+   const toggle = () => {
+      setIsOpen(!isOpen)
+   }
+
+   const menuOpen = () => {
       return (
-         <ul className='h-24 space-x-5 pr-4 uppercase tracking-wider text-gray-700 text-2xl hidden'></ul>
+         <ul className='px-4 pt-2 pb-4'>
+            {/* <ul className='h-24 flex justify-end items-center space-x-5 pr-4'> */}
+            {/* <ul className='flex flex-col items-center space-y-5 uppercase tracking-wider text-gray-700 text-2xl'> */}
+            {navbarMapper()}
+         </ul>
       )
    }
 
@@ -32,16 +42,27 @@ const Navbar = () => {
    }
 
    return (
-      <>
+      <header>
          {location.pathname !== '/home' && (
-            <nav className='w-full h-24 bg-red bg-opacity-75 border-b-2'>
-               <ul className='h-24 flex justify-end items-center space-x-5 pr-4'>
-                  {/* <ul className='flex flex-col items-center space-y-5 uppercase tracking-wider text-gray-700 text-2xl'> */}
-                  {navbarMapper()}
+            <nav className='w-full bg-opacity-75 border-b-2'>
+               <ul className='flex items-center justify-between px-5 py-3'>
+                  <>
+                     <img src={Logo} alt='Logo' className='h-20' />
+                  </>
+                  <>
+                     <button
+                        type='button'
+                        className='block text-gray-700 hover:text-grey-300 focus:text-gray-300 focus:outline-none'
+                        onClick={toggle}
+                     >
+                        <AiOutlineBars size={28} className='fill-current' />
+                     </button>
+                  </>
                </ul>
+               {isOpen ? menuOpen() : <></>}
             </nav>
          )}
-      </>
+      </header>
    )
 }
 
