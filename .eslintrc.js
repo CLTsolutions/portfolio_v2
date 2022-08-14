442ac1cdfd83c1e8d790d54a904769f33bb5bf12
Chelsey T <73909880+CLTsolutions@users.noreply.github.com>
2022-08-14 00:07:42 -0400

    Add Eslint and prettier config files

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    // 'max-len': [1, { code: 120 }],
+    'newline-after-var': [1, 'always'],
+    'object-curly-newline': [0],
+    'padding-line-between-statements': [1,
+      // { blankLine: 'always', prev: '*', next: ['const', 'let', 'var'] },
+      { blankLine: 'always', prev: ['const', 'let', 'var'], next: 'block' },
+      { blankLine: 'always', prev: '*', next: 'return' },
+      { blankLine: 'always', prev: '*', next: ['class', 'if', 'while', 'switch', 'try'] },
+    ],
+    'react/jsx-curly-spacing': [1, {
+      when: 'always',
+      spacing: {
+        objectLiterals: 'always',
+      },
+    }],
+    'react/react-in-jsx-scope': 'off',
+    'react/sort-prop-types': [1, {
+      requiredFirst: true,
+      sortShapeProp: true,
+    }],
+    semi: [1],
+  },
+};
